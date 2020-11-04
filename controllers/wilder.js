83b905dc4eda99b9b50dd4911d431af1a1bc289b
Arnaud Renaud <arnaud.renaud@gmail.com>
2020-11-04 09:59:26 +0100

    Add PUT method to update wilder

--- controllers/wilder.js
+++ controllers/wilder.js
@@ -23,4 +23,16 @@ module.exports = {
         });
     });
   },
+  update: (req, res) => {
+    const id = req.params.id;
+    WilderModel.findOneAndUpdate({ _id: id }, req.body, {
+      new: true,
+    })
+      .then((result) => {
+        res.json({ result });
+      })
+      .catch((err) => {
+        res.status(500).json({ err });
+      });
+  },
 };
--- index.js
+++ index.js
@@ -21,6 +21,7 @@ server.get("/", (req, res) => {
 
 server.post("/api/wilders", wilderControllers.create);
 server.get("/api/wilders", wilderControllers.get);
+server.put("/api/wilders/:id", wilderControllers.update);
 
 server.listen(3000, () => {
   console.log("Server listening on port 3000.");
